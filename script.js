61fe9abee457637c3628f102e065e5b130d6e93d
Zhenya <115710959+ZhenyaLysachenko@users.noreply.github.com>
2023-01-13 22:14:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = "Zhenya";
+let admin;
+admin = name;
+console.log(admin)
+
+let days = 3;
+let hoursInDay = 24;
+let secondsInMinute = 60;
+let minutesInHour = 60;
+
+let daysSecond = days * (24 * 60 * 60);
+console.log(daysSecond);
+
+let text = prompt("How are you?")
+console.log(text)
+
+
